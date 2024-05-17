0a881804b148b58d53a74864d1908be1fd74cbd9
Rohit Rai <git-user>
2024-05-17 11:29:30 +0530

    creating simple ec2 instance

---
+++ day-1/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-021ae75bdd155a4d5"
+    instance_type = "t3.micro"
+    key_name = "rrai-key"
+    vpc_security_group_ids = ["sg-02b9e021054634ee7"]
+  
+}
