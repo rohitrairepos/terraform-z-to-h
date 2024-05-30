807c2d31947b50cae03466056af27cb88dd7232a
Rohit Rai <git-user>
2024-05-30 22:10:29 +0530

    update

---
+++ day-3/ec2_instance-data-output-public-ip/variable.tf
@@ -0,0 +1,14 @@
+variable "ami-type-us-east-1" {
+    default = "t2.mciro"
+  
+}
+
+variable "ami-type-us-west-1" {
+    default = "t2.mciro"
+  
+}
+
+variable "key_name" {
+    default = "rrai-key"
+  
+}
