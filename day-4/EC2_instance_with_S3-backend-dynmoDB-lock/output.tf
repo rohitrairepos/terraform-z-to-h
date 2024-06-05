c6aa7f12db7c68065fec163ee72f64952ecc83d0
Rohit Rai <git-user>
2024-06-05 16:03:25 +0530

    update

---
+++ day-4/EC2_instance_with_S3-backend-dynmoDB-lock/output.tf
@@ -0,0 +1,25 @@
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-*-20240423"]
+    } 
+
+   filter {
+     name   = "virtualization-type"
+     values = ["hvm"]
+   }
+   
+
+    owners = ["099720109477"]
+}
+
+data "aws_security_group" "default_sec_group" {
+    filter {
+      name = "group-name"
+      values = ["*default*"]
+    }
+
+  
+}
---
+++ day-4/EC2_instance_with_S3-backend-dynmoDB-lock/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.52.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform.freetier08"
+    key = "terraform-folders/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
